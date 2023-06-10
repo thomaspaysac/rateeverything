ba16bea0dfc9fe81d77b1a52622bd90665169d13
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-06-10 08:21:18 +0200

    Artist page styling

--- src/components/artist_page/ReleasesList.js
+++ src/components/artist_page/ReleasesList.js
@@ -5,15 +5,18 @@ const data = await getReleases('Metallica');
 
 const AlbumList = data.map((el) => {
   return (
-  <div key={el.artist + '_' + el.release} className='release-list_release'>  
-    <div>
-      <div key='release-name'>{el.release}</div>
-      <div key='year'>{el.year}</div>
-    </div>
-    <div key='release_reviews'>{el.reviews.length}</div>
-    <div key='release_ratings'>{el.ratings.length}</div>
-    <div key='release_average'>{el.average.toFixed(2)}</div>
-  </div>
+  <tr key={el.artist + '_' + el.release} className='release-list_release'>
+    <td>
+      O
+    </td>
+    <td>
+      <div key='release_title' className='release-table_title'>{el.release}</div>
+      <div key='release_year' className='release-table_year'>{el.year}</div>
+    </td>
+    <td key='release_reviews' className='td-fixed release-table_reviews'>{el.reviews.length}</td>
+    <td key='release_ratings' className='td-fixed release-table_ratings'>{el.ratings.length}</td>
+    <td key='release_average' className='td-fixed release-table_average'>{el.average.toFixed(2)}</td>
+  </tr>
   )
 });
 
@@ -25,21 +28,36 @@ const ReleasesList = () => {
       <h2>Discography</h2>
       <div className='discography'>
         <div className='release-type'>Album</div>
-        <div className='disco-header'>
-          <div>Title / Release Date</div>
-          <div>Reviews</div>
-          <div>Ratings</div>
-          <div>Average</div>
-        </div>
-        <div className='disco-list'>
-          <div>
+        <table className='releases-list_table'>
+          <thead className='releases-list_thead'>
+            <tr>
+              <th> </th>
+              <th className='th-title'>Title / Release Date</th>
+              <th className='th-fixed'>Reviews</th>
+              <th className='th-fixed'>Ratings</th>
+              <th className='th-fixed'>Average</th>
+            </tr>
+          </thead>
+          <tbody>
             {AlbumList}
-          </div>
-        </div>
+          </tbody>
+        </table>
       </div>
     </div>
     
   );
 }
+
+/*<div className='disco-header'>
+<div>Title / Release Date</div>
+<div>Reviews</div>
+<div>Ratings</div>
+<div>Average</div>
+</div>
+<div className='disco-list'>
+<div>
+  {AlbumList}
+</div>
+</div>*/
 
 export default ReleasesList;
