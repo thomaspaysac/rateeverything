dd47db8fb831223cb45cc237b956573c8e9110cf
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-06-18 07:53:09 +0200

    Add detailled tracklist on release submit form

--- src/components/NewRelease.js
+++ src/components/NewRelease.js
@@ -1,18 +1,69 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { submitRelease } from '../functions';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const NewReleasePage = () => {
+  const [trackList, setTrackList] = useState([]);
+  
   const urlParams = useParams();
 
-  const sendForm = (e) => {
+  const addTrack = () => {
+    const temp = trackList.slice();
+    setTrackList([...temp, {number: '', title: '', time: ''}])
+  }
+
+  const removeTrack = (n) => {
+    const temp = trackList.slice();
+    temp.splice(n, 1);
+    setTrackList([...temp]);
+  }
+
+  const updateTrackNumber = (e, n) => {
+    const temp = trackList.slice();
+    temp[n].number = e.target.value;
+    setTrackList([...temp]);
+  }
+
+  const updateTrackTitle = (e, n) => {
+    const temp = trackList.slice();
+    temp[n].title = e.target.value;
+    setTrackList([...temp]);
+  }
+
+  const updateTrackTime = (e, n) => {
+    const temp = trackList.slice();
+    temp[n].time = e.target.value;
+    setTrackList([...temp]);
+  }
+
+  const trackForm = (n) => {
+    return (
+      <div key={`track-input-${n}`} className="input-group">
+        <input type='button' onClick={() => removeTrack(n)} value='-' />
+        <input type='text' 
+          name={`track${n}_number`} 
+          onChange={(e) => updateTrackNumber(e, n)} 
+          value={trackList[n].number} />
+        <input type='text' 
+          name={`track${n}_title`} 
+          onChange={(e) => updateTrackTitle(e, n)} 
+          value={trackList[n].title} />
+        <input type='text'
+          name={`track${n}_time`}
+          onChange={(e) => updateTrackTime(e, n)}
+          value={trackList[n].time} />
+      </div>
+    )
+  }
+
+   const sendForm = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
+    console.log(data);
     const tracklist = data.tracklist.split(',');
     const genres = data.genres.split(',');
-    submitRelease(urlParams.artist, data.title, data.year, tracklist, genres, [], [])
+    submitRelease(urlParams.artist, data.title, data.year, trackList, genres, [], [])
   }
 
   return (
@@ -28,6 +79,17 @@ const NewReleasePage = () => {
           <label htmlFor='tracklist'>Tracklist:</label>
           <input type='text' name='tracklist'/>
           <input type='submit' value='Submit' />
+          <div>Tracklist :
+          <input type='button' onClick={() => addTrack()} value='Add track' />
+            {
+              trackList.map((el, i) => {
+                return (
+                  trackForm(i)
+                )
+              })
+            }
+            <button onClick={() => console.log(trackList)}>Log</button>
+          </div>
         </form>
       </div>
     </div>
